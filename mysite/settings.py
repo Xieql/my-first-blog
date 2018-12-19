b63a1f4a6a2fd73c5c1d8bc80d2989ac8ec27a71
Xieql <727092844@qq.com>
2018-12-19 19:18:15 +0800

    add host head

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
